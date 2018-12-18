5dcd0694c99224cc76630407306f86e3f096d360
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-12-18 02:14:35 -0800

    [add] Added concentration cards and show emojis

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var flipCount = 0 {
+        didSet {
+             flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
 
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices: Array<String> = ["❄️","⛄️","❄️","⛄️"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            print("cardNumber = \(cardNumber)")
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+        
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9900344014, green: 0.7853665948, blue: 0.5426693559, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.7601440549, green: 0.7730435729, blue: 0.4992948174, alpha: 1)
+        }
+    }
+    
 
 }
 
